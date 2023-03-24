f00cee21d935f84fa9a88a2e084613225be46141
EDEC388 <EDEC388@WSAMZN-J1UFQTA4>
2023-03-24 13:55:13 -0500

    Add project files.

---
+++ Globals.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using Newtonsoft.Json.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace DeveloperCert
+{
+
+    internal class Globals
+    {
+        private static string token;
+        private static string lnId = "";
+
+        public static string GetAccessToken()
+        {
+            return token;
+        }
+
+        public static async Task SetAccessToken(string un, string pw)
+        {
+            var options = new RestClientOptions("https://api.elliemae.com")
+            {
+                MaxTimeout = -1,
+            };
+            var client = new RestClient(options);
+            var request = new RestRequest("/oauth2/v1/token", Method.Post);
+            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+            request.AddParameter("grant_type", "password");
+            request.AddParameter("username", "iceStudent"+"@encompass:BE11166969");
+            request.AddParameter("password", "student@DEV22");
+            request.AddParameter("client_id", "ruc51qr");
+            request.AddParameter("client_secret", "ysli#MSdlbyHngOyctiJmK$0l82^9!o$dzSwvhW612!GPoi38loF4CFQ8ZiJTu$@");
+            RestResponse response = await client.ExecuteAsync(request);
+            //Console.WriteLine(response.Content);
+            //Console.ReadLine();
+            JObject respContent = JObject.Parse(response.Content);
+            //Instantiate a JToken object with the extracted access token
+            JToken aToken = respContent.SelectToken("$.access_token");
+            //Set the token variable to the extracted access token as a string
+            token = aToken.ToString();
+        }
+        public static string GetLoanId()
+        {
+            return lnId;
+        }
+        public static void SetLoanId(string id)
+        {
+            id = lnId;
+        }
+        public static async Task CreatePurchaseLoan(string fname, string lname, string ppamt, string dppercent, string noterate, string term)
+        {
+            var options = new RestClientOptions("https://api.elliemae.com")
+            {
+                MaxTimeout = -1,
+            };
+            var client = new RestClient(options);
+            var request = new RestRequest("/encompass/v3/loans?loanFolder=DevEssentialsCert&view=entity", Method.Post);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("Authorization", "Bearer " + token);
+            var body = @"{" + "\n" +
+            @"    ""applications"": [" + "\n" +
+            @"        {" + "\n" +
+            @"            ""borrower"": {" + "\n" +
+            @"                ""firstName"": """ + fname + @" "", " + "\n" +
+            @"                ""lastName"": """ + lname + @" "" "+"\n"+ 
+            @"            }" + "\n" +
+            @"        }" + "\n" +
+            @"    ]," + "\n" +
+            @"    ""property"": {" + "\n" +
+            @"        ""loanPurposeType"": ""Cash-Out Refinance""" + "\n" +
+            @"    }," + "\n" +
+            @"    ""purchasePriceAmount"": """ + ppamt + @" ""," + "\n" +
+            @"    ""downPaymentPercent"": """ + dppercent + @" ""," + "\n" +
+            @"    ""requestedInterestRatePercent"": """ + noterate + @" ""," + "\n" +
+            @"    ""loanAmortizationTermMonths"": """ + term + @" "" "+ "\n" +
+            @"" + "\n" +
+            @"    }" + "\n" +
+            @"}";
+            request.AddStringBody(body, DataFormat.Json);
+            RestResponse response = await client.ExecuteAsync(request);
+            Console.WriteLine(response.Content);
+            //Parse the response content
+            JObject respContent = JObject.Parse(response.Content);
+            //Instantiate a JToken object with the extracted lnId
+            JToken loanGuid = respContent.SelectToken("$.id");
+            //Set the lnId variable to the extracted lnId as a string
+            lnId = loanGuid.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+using static DeveloperCert.Globals;
+
+namespace DeveloperCert
+{
+    class Program
+    {
+        private static string userName, password;
+        private static string accessToken;
+        private static string firstName, lastName, pp, dpPercent, rate,term;
+        private static string loanId;
+        static void Main(string[] args)
+        {
+            CompleteAuthentication();
+            CreateLoan();
+        }
+
+        private static void CreateLoan()
+        {
+            Console.WriteLine("First Name:");
+            firstName = Console.ReadLine();
+            Console.WriteLine("Last Name:");
+            lastName = Console.ReadLine();
+            Console.WriteLine("Purchase Price:");
+            pp = Console.ReadLine();
+            Console.WriteLine("Down Payment Percentage:");
+            dpPercent = Console.ReadLine();
+            Console.WriteLine("Note Rate:");
+            rate = Console.ReadLine();
+            Console.WriteLine("Loan Term:");
+            term = Console.ReadLine();
+            var task2 = CreatePurchaseLoan(firstName, lastName, pp, dpPercent, rate, term);
+            Task.WaitAll(task2);
+            loanId = GetLoanId();
+            Console.WriteLine("\nThe loan ID (GUID) is: " + loanId);
+            Console.ReadLine();
+        }
+        private static void CompleteAuthentication()
+        {
+            Console.WriteLine("User Name:");
+            userName = Console.ReadLine();
+            Console.WriteLine("Password:");
+            password = Console.ReadLine();
+
+            var task1 = SetAccessToken(userName, password);
+
+            Task.WaitAll(task1);
+            accessToken = GetAccessToken();
+            Console.WriteLine("The access token is: " + accessToken);
+            Console.ReadLine();
+        }
+        
+    }
+}
